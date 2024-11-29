4abb638f1b508d2750f474c06247d9cfc0bf8885
Hieule <vanhieu2241998@gmail.com>
2024-11-29 13:20:20 +0700

    Enhance Profile component: add user state management and logout functionality

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,21 +1,25 @@
-import React, { useCallback } from "react";
+import React, { use, useCallback } from "react";
 
 import { redirect } from "next/navigation";
 
 import { Dropdown, IDropdownOption } from "@components/Dropdown";
 import { PowerSettingsNew, SettingsOutlined } from "@mui/icons-material";
 import { Avatar, IconButton } from "@mui/material";
+import { useAppStore } from "@providers/AppStoreProvider";
 import userService from "@services/user";
 import { triggerToastDev } from "@utils/index";
 import Cookies from "js-cookie";
 
 export const Profile = () => {
+    const { setUserInfo } = useAppStore((state) => state);
+
     const handleLogout = useCallback(async () => {
         await userService.userLogout();
 
         //clear token
         localStorage.removeItem("access-token");
         Cookies.remove("refresh-token");
+        setUserInfo(null);
 
         redirect("/login");
     }, []);
