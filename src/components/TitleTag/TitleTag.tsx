ffb309466066b52936697e07b46a96c966302b0c
Hieule <vanhieu2241998@gmail.com>
2024-12-09 13:14:15 +0700

    feat: update TitleTag component to support color variants and improve styling

--- src/components/TitleTag/TitleTag.tsx
+++ src/components/TitleTag/TitleTag.tsx
@@ -5,24 +5,36 @@ import { Typography, styled } from "@mui/material";
 const StyledTitleTag = styled("div")`
     font-size: 20px;
     font-weight: bold;
-    color: white;
+    color: #667085;
+    background: white;
     height: 45px;
     min-width: 100px;
-    background: var(--palette-primary-main);
     border-radius: 45px;
     line-height: 45px;
     box-shadow: 0px 4px 4px 0px #00000040;
     text-align: center;
     padding: 0 16px;
+
+    &.primary {
+        background: var(--palette-primary-main);
+        color: white;
+    }
+
+    &.default {
+        background: white;
+        color: #667085;
+        border: 1px solid var(--input-border-color);
+    }
 `;
 
 interface TitleTagProps {
     title?: string;
+    color?: "primary" | "default";
 }
 
-export const TitleTag = ({ title }: TitleTagProps) => {
+export const TitleTag = ({ title, color = "primary" }: TitleTagProps) => {
     return (
-        <StyledTitleTag>
+        <StyledTitleTag className={color}>
             <Typography variant='h6' noWrap fontWeight={"bold"} lineHeight={"45px"}>
                 {title}
             </Typography>
