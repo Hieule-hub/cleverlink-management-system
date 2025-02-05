db67c1ac96772d980f78d173131f04de28bd13fb
Hieule <vanhieu2241998@gmail.com>
2025-02-05 14:24:44 +0700

    disabled sort total device in company table

--- src/modules/Company/Company.tsx
+++ src/modules/Company/Company.tsx
@@ -130,7 +130,6 @@ export const CompanyPage = () => {
                 dataIndex: "totalDevices",
                 align: "center",
                 width: 200,
-                sorter: true,
                 render: (text) => text
             },
             {
